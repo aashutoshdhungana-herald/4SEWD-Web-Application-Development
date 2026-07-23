64b2ceb8b31b4b57a26e0828621926189c533bbf
Aashutosh Dhungana <aashutosh.dhungana@heraldcollege.com.np>
2026-07-23 12:25:24 +0545

    Refactor App component by removing unused imports and enhance navigation styles in index.css

--- todo-app-initial/src/App.jsx
+++ todo-app-initial/src/App.jsx
@@ -1,10 +1,6 @@
 import PageTitle from "./components/PageTitle/PageTitle";
 import TaskContainer from "./components/TaskContainer/TaskContainer";
 import Form from "./components/Form/Form";
-import NavBar from "./components/NavBar/NavBar";
-import Home from "./pages/Home";
-import AddTodo from "./pages/AddTodo";
-import TodoItem from "./pages/TodoItem";
 
 function App() {
   return (
